543379c08f1b49a6fa0524d8257aa10b8d8d3b3b
Mohamed boye Jallo Jamboria <43971170+Scanafrik@users.noreply.github.com>
2018-10-26 15:21:51 +0200

    Copyright © 2018 ScanAfrik Consultancy
    ScanAfrik Consultancy copyrights

--- wp-content/themes/twentyfourteen/footer.php
+++ wp-content/themes/twentyfourteen/footer.php
@@ -5,8 +5,8 @@
  * Contains footer content and the closing of the #main and #page div elements.
  *
  * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
+ * @subpackage Twenty8teen
+ * @since Twenty8teen 1.0
  */
 ?>
 
@@ -17,14 +17,14 @@
 			<?php get_sidebar( 'footer' ); ?>
 
 			<div class="site-info">
-				<?php do_action( 'twentyfourteen_credits' ); ?>
+				<?php do_action( 'twentyeighteen_credits' ); ?>
 				<?php
 				if ( function_exists( 'the_privacy_policy_link' ) ) {
 					the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
 				}
 				?>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfourteen' ) ); ?>" class="imprint">
-					<?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyeighteen' ) ); ?>" class="imprint">
+					<?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'ScanAfrik Consultancy' ); ?>
 				</a>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
